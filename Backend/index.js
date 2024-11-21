388974c5ae047b3bc7c905999e956ba8d7da2a62
Tanpreet Singh <121126869+iamtsanand@users.noreply.github.com>
2024-11-22 02:14:04 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,28 +6,30 @@ import cors from "cors";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
-
-const PORT = process.env.PORT || 4000;
+// Use Vercel's dynamic port
+const PORT = process.env.PORT || 3000;
 const URI = process.env.MongoDBURI;
 
-// connect to mongoDB
-try {
-    mongoose.connect(URI);
+// Connect to MongoDB
+mongoose.connect(URI)
+  .then(() => {
     console.log("Connected to mongoDB");
-} catch (error) {
+  })
+  .catch(error => {
     console.log("Error: ", error);
-}
+  });
 
-// defining routes
+// Defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
